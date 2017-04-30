b3074026c948ecfe2488fa966cb1028650031c0a
Lee Keitel <lee@keitel.xyz>
2017-04-29 22:12:08 -0500

    Implemented setting verifier

---
+++ config/verifier.go
@@ -0,0 +1,127 @@
+package config
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/lfkeitel/spartan/utils"
+)
+
+// SettingType defines the type of a setting
+type SettingType int
+
+const (
+	// Any skips type verification
+	Any SettingType = iota
+	// String setting
+	String
+	// Int setting (golang's int type)
+	Int
+	// Float setting (golang's float64 type)
+	Float
+	// Bool setting (true/false)
+	Bool
+	// Map setting
+	Map
+	// Array setting (elements can be Int, String, or Array)
+	Array
+)
+
+// A Setting defines a single setting for a module
+type Setting struct {
+	// Name is used for the root or map key names
+	Name     string
+	Required bool
+
+	// If value is nil, it will be assigned Default. Default is NOT verified
+	// to be the right type for the setting. It's up to the module to ensure
+	// the default value is valid.
+	Default interface{}
+	Type    SettingType
+
+	// ElemType defines the type of elements in an array. The only setting that's
+	// used is Type. Arrays can only consist of Strings, Ints, or another array of
+	// the former.
+	ElemType *Setting
+
+	// MapDef defines the key:value pairs of a Map type setting.
+	MapDef []Setting
+}
+
+// VerifySettings is used to check an InterfaceMap against a schema of Setting values.
+func VerifySettings(data utils.InterfaceMap, settings []Setting) error {
+	if len(settings) == 0 {
+		return nil
+	}
+
+	for _, setting := range settings {
+		v, exists := data.GetOK(setting.Name)
+		if !exists && setting.Required {
+			return fmt.Errorf("Expected required setting %s", setting.Name)
+		}
+
+		// If there's no value, no need to continue verification
+		if v == nil {
+			data.Set(setting.Name, setting.Default)
+			continue
+		}
+
+		switch setting.Type {
+		case Any:
+			continue
+		case String:
+			if _, ok := v.(string); !ok {
+				return fmt.Errorf("Setting %s expected string, got %T", setting.Name, v)
+			}
+		case Int:
+			if _, ok := v.(int); !ok {
+				return fmt.Errorf("Setting %s expected int, got %T", setting.Name, v)
+			}
+		case Float:
+			if _, ok := v.(float64); !ok {
+				return fmt.Errorf("Setting %s expected float, got %T", setting.Name, v)
+			}
+		case Bool:
+			if _, ok := v.(bool); !ok {
+				return fmt.Errorf("Setting %s expected true/false, got %T", setting.Name, v)
+			}
+		case Array:
+			if err := verifyArray(v, setting.ElemType); err != nil {
+				return err
+			}
+		case Map:
+			m, ok := v.(utils.InterfaceMap)
+			if !ok {
+				return fmt.Errorf("Setting %s expected map of values", setting.Name)
+			}
+			if err := VerifySettings(m, setting.MapDef); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func verifyArray(data interface{}, elemType *Setting) error {
+	if a, ok := data.([]interface{}); ok && len(a) == 0 {
+		return nil
+	}
+
+	switch elemType.Type {
+	case String:
+		if _, ok := data.([]string); !ok {
+			return errors.New("Expected array of string")
+		}
+	case Int:
+		if _, ok := data.([]int); !ok {
+			return errors.New("Expected array of int")
+		}
+	case Array:
+		return verifyArray(data, elemType)
+	default:
+		return errors.New("invalid array element type")
+	}
+
+	return nil
+}
---
+++ config/verifier_test.go
@@ -0,0 +1,139 @@
+package config
+
+import (
+	"testing"
+
+	"github.com/lfkeitel/spartan/utils"
+)
+
+func TestNoSettings(t *testing.T) {
+	m := utils.NewInterfaceMap()
+
+	if err := VerifySettings(m, nil); err != nil {
+		t.Errorf("Unexpected error %s", err)
+	}
+}
+
+func TestSimpleSettings(t *testing.T) {
+	m := utils.NewInterfaceMap()
+	m.Set("field", "message")
+	m.Set("action", "delete")
+
+	settings := []Setting{
+		{
+			Name:     "field",
+			Type:     String,
+			Required: true,
+		},
+		{
+			Name:     "action",
+			Type:     String,
+			Required: false,
+			Default:  "reverse",
+		},
+	}
+
+	if err := VerifySettings(m, settings); err != nil {
+		t.Errorf("Unexpected error %s", err)
+	}
+
+	m.Set("action", 42)
+
+	if err := VerifySettings(m, settings); err == nil {
+		t.Error("Error not received. Expected invalid setting type")
+	}
+}
+
+func TestDefaultSettings(t *testing.T) {
+	m := utils.NewInterfaceMap()
+	m.Set("field", "message")
+
+	settings := []Setting{
+		{
+			Name:     "field",
+			Type:     String,
+			Required: true,
+		},
+		{
+			Name:    "action",
+			Type:    String,
+			Default: "reverse",
+		},
+	}
+
+	VerifySettings(m, settings)
+
+	v := m.Get("action").(string)
+	if v != "reverse" {
+		t.Errorf("Incorrect default value. Expected reverse, got %s", v)
+	}
+}
+
+func TestArraySetting(t *testing.T) {
+	m := utils.NewInterfaceMap()
+	m.Set("fields", []string{"message", "type"})
+
+	settings := []Setting{
+		{
+			Name:     "fields",
+			Type:     Array,
+			ElemType: &Setting{Type: String},
+			Required: true,
+		},
+	}
+
+	if err := VerifySettings(m, settings); err != nil {
+		t.Errorf("Unexpected error %s", err)
+	}
+
+	m.Set("fields", []int{42, 43})
+	if err := VerifySettings(m, settings); err == nil {
+		t.Error("Expected invalid element type error")
+	}
+}
+
+func TestMapSetting(t *testing.T) {
+	m := utils.NewInterfaceMap()
+	m.Set("fields", utils.InterfaceMap{
+		"message": true,
+		"type":    false,
+	})
+	m.Set("actions", utils.InterfaceMap{
+		"random":  "yep",
+		"number?": 42,
+	})
+
+	settings := []Setting{
+		{ // Test full verification of a map
+			Name: "fields",
+			Type: Map,
+			MapDef: []Setting{
+				{
+					Name: "message",
+					Type: Bool,
+				},
+				{
+					Name: "type",
+					Type: Bool,
+				},
+			},
+			Required: true,
+		},
+		{ // Simple test that it is a map
+			Name: "actions",
+			Type: Map,
+		},
+	}
+
+	if err := VerifySettings(m, settings); err != nil {
+		t.Errorf("Unexpected error %s", err)
+	}
+
+	m.Set("fields", utils.InterfaceMap{
+		"message": true,
+		"type":    42,
+	})
+	if err := VerifySettings(m, settings); err == nil {
+		t.Error("Expected invalid element type error")
+	}
+}
