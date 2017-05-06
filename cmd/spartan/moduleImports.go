5b4192992346a9255d7245187e863f919e97bc00
Lee Keitel <lee@keitel.xyz>
2017-05-06 13:22:49 -0500

    Rename plugin repositories

--- cmd/spartan/moduleImports.go
+++ cmd/spartan/moduleImports.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	// Imports
-	_ "github.com/spartanlogs/spartan-file-input"
-	_ "github.com/spartanlogs/spartan-generator-input"
+	_ "github.com/spartanlogs/spartan-input-file"
+	_ "github.com/spartanlogs/spartan-input-generator"
 
 	// Filters
-	_ "github.com/spartanlogs/spartan-date-filter"
-	_ "github.com/spartanlogs/spartan-grok-filter"
-	_ "github.com/spartanlogs/spartan-metrics-filter"
-	_ "github.com/spartanlogs/spartan-remove-field-filter"
+	_ "github.com/spartanlogs/spartan-filter-date"
+	_ "github.com/spartanlogs/spartan-filter-grok"
+	_ "github.com/spartanlogs/spartan-filter-metrics"
+	_ "github.com/spartanlogs/spartan-filter-remove-field"
 
 	// Outputs
-	_ "github.com/spartanlogs/spartan-stdout-output"
+	_ "github.com/spartanlogs/spartan-output-stdout"
 )
