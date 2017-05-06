59940827bae52a24a49c59bbb687f9ac21efdeba
Lee Keitel <lee@keitel.xyz>
2017-05-06 15:40:10 -0500

    Fixed SMA test

--- metrics/sma_test.go
+++ metrics/sma_test.go
@@ -9,7 +9,7 @@ func TestSMAAddRate(t *testing.T) {
 	sma := newSMA(1*time.Minute, 5*time.Second)
 	sma.Add(1000)
 	sma.Tick()
-	if rate := sma.Rate(); rate != 200.0 {
-		t.Errorf("Wrong rate. Expected %f, got %f", 1.0, rate)
+	if rate := sma.Rate(); rate != 100.0 {
+		t.Errorf("Wrong rate. Expected %f, got %f", 100.0, rate)
 	}
 }
